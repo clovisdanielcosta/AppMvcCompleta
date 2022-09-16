adecd30330a956a5f8a8ab8b5aab404b193016f1
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-09-16 14:55:25 -0300

    Updated interface IFornecedorRepository and class Repository

--- src/CD.Data/Repository/Repository.cs
+++ src/CD.Data/Repository/Repository.cs
@@ -1,11 +1,6 @@
 ﻿using AppMvcBasica.Models;
 using CD.Business.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CD.Data.Repository
 {

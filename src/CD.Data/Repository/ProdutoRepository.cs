8711bd3c1354d750170b6d7b995a7d1f3b5f9cac
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-09-16 15:58:13 -0300

    Injected dependency on Repository and updated ProdutoRepository to send parameter context to base class

--- src/CD.Data/Repository/ProdutoRepository.cs
+++ src/CD.Data/Repository/ProdutoRepository.cs
@@ -1,16 +1,18 @@
 ﻿using AppMvcBasica.Models;
 using CD.Business.Interfaces;
+using CD.Data.Context;
 using Microsoft.EntityFrameworkCore;
 
 namespace CD.Data.Repository
 {
     public class ProdutoRepository : Repository<Produto>, IProdutoRepository
     {
+        public ProdutoRepository(MeuDbContext context) : base(context){}
         public async Task<Produto> ObterProdutoFornecedor(Guid id)
         {
             return await Db.Produtos.AsNoTracking().Include(f => f.Fornecedor).FirstOrDefaultAsync(p => p.Id == id);
         }
-          
+            
         public async Task<IEnumerable<Produto>> ObterProdutosFornecedores()
         {
             return await Db.Produtos.AsNoTracking().Include(f => f.Fornecedor).OrderBy(p => p.Nome).ToListAsync();

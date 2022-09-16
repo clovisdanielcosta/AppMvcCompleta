9a2c2d9e087197d3505a36c6b5730f29891a3f07
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-09-16 14:28:49 -0300

    Created generic interface IRepository and interface IFornecedorRepository

---
+++ src/CD.Business/Interfaces/IFornecedorRepository.cs
@@ -0,0 +1,15 @@
+﻿using AppMvcBasica.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD.Business.Interfaces
+{
+    internal interface IFornecedorRepository : IRepository<Fornecedor>
+    {
+        Task<Fornecedor> ObterFornecedorEndereco(Guid Id);
+        Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid Id);
+    }
+}

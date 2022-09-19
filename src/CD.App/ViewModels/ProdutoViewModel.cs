23b6bf47c0122bb200948832b1fff31448df5cc0
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-09-19 14:33:39 -0300

    Formatting views in Produtos and Fornecedores

--- src/CD.App/ViewModels/ProdutoViewModel.cs
+++ src/CD.App/ViewModels/ProdutoViewModel.cs
@@ -12,6 +12,7 @@ namespace CD.App.ViewModels
         [DisplayName("Fornecedor")]
         public Guid FornecedorId { get; set; }
 
+        [DisplayName("Nome do Produto")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Nome { get; set; }
@@ -21,6 +22,7 @@ namespace CD.App.ViewModels
         [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
 
+        [DisplayName("Imagem do Produto")]
         public IFormFile ImagemUpload { get; set; }
 
         public string Imagem { get; set; }

7da954ae5ee50b79fedde7052154fc9286f89dc1
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-09-20 14:51:49 -0300

    Created method AtualizarEndereco on FornecedoresController

--- src/CD.App/Controllers/FornecedoresController.cs
+++ src/CD.App/Controllers/FornecedoresController.cs
@@ -99,6 +99,18 @@ namespace CD.App.Controllers
             return RedirectToAction("Index");
         }
 
+        public async Task<IActionResult> AtualizarEndereco(Guid id)
+        {
+            var fornecedor = await ObterFornecedorEndereco(id);
+
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            return PartialView("_AtualizarEndereco", new FornecedorViewModel { Endereco = fornecedor.Endereco });
+        }
+
         private async Task<FornecedorViewModel> ObterFornecedorEndereco(Guid id)
         {
             return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorEndereco(id));

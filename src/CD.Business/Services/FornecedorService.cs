1e9884e8962c6d08e7eab2894d10749aff90eca7
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-09-21 15:37:50 -0300

    Created class EnderecoValidation and updated class FornecedorService

--- src/CD.Business/Services/FornecedorService.cs
+++ src/CD.Business/Services/FornecedorService.cs
@@ -1,26 +1,29 @@
 ﻿using CD.Business.Interfaces;
 using CD.Business.Models;
+using CD.Business.Models.Validations;
 
 namespace CD.Business.Services
 {
     public class FornecedorService : BaseService, IFornecedorService
     {
-        public Task Adicionar(Fornecedor fornecedor)
+        public async Task Adicionar(Fornecedor fornecedor)
         {
-            throw new NotImplementedException();
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)
+                && !ExecutarValidacao(new EnderecoValidation(), fornecedor.Endereco)) return;
         }
 
-        public Task Atualizar(Fornecedor fornecedor)
+        public async Task Atualizar(Fornecedor fornecedor)
         {
-            throw new NotImplementedException();
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)) return;
+
         }
 
-        public Task AtualizarEndereco(Endereco endereco)
+        public async Task AtualizarEndereco(Endereco endereco)
         {
-            throw new NotImplementedException();
+            if (!ExecutarValidacao(new EnderecoValidation(), endereco)) return;
         }
 
-        public Task Remover(Guid id)
+        public async Task Remover(Guid id)
         {
             throw new NotImplementedException();
         }

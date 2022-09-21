afb499319e2e900fa9e5c435096faccc18d5d778
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-09-21 15:30:05 -0300

    Updated class BaseService

--- src/CD.Business/Services/BaseService.cs
+++ src/CD.Business/Services/BaseService.cs
@@ -1,7 +1,34 @@
-﻿namespace CD.Business.Services
+﻿using CD.Business.Models;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace CD.Business.Services
 {
     public abstract class BaseService
     {
+        protected void Notificar(ValidationResult validationResult)
+        {
+            foreach (var error in validationResult.Errors)
+            {
+                Notificar(error.ErrorMessage);
+            }
+        }
+
+        protected void Notificar(string mensagem)
+        {
+            
+        }
+
+        protected bool ExecutarValidacao<TV, TE>(TV validacao, TE entidade) where TV : AbstractValidator<TE> where TE : Entity
+        {
+            var validator = validacao.Validate(entidade);
+
+            if (validator.IsValid) return true;
+
+            Notificar(validator);
+
+            return false;
+        }
     }
 
 }

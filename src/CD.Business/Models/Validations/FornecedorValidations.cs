3f3fb7a4c1c099cc052b25143f84a124f9a5db5f
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-09-21 15:02:27 -0300

    Added FluentValidation and create classes FornecedorValidations and ValidacaoDocs

---
+++ src/CD.Business/Models/Validations/FornecedorValidations.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace CD.Business.Models.Validations
+{
+    public class FornecedorValidations : AbstractValidator<Fornecedor>
+    {
+        public FornecedorValidations()
+        {
+            RuleFor(f => f.Nome)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser digitado")
+                .Length(2, 100)
+                .WithMessage("O campo {PropertyName} precisa ter entre {MinLenght} e {MaxLenght} caracteres");
+        }
+    }
+}

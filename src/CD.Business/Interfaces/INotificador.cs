5aa023fd79acaac5bfc6a0fccf75ebbc2562020c
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-09-22 10:47:51 -0300

    Created interface INotificador and classes Notificador and Notificacao

---
+++ src/CD.Business/Interfaces/INotificador.cs
@@ -0,0 +1,11 @@
+﻿using CD.Business.Notifications;
+
+namespace CD.Business.Interfaces
+{
+    internal interface INotificador
+    {
+        bool TemNotificacao();
+        List<Notificacao> ObterNotificacoes();
+        void Handle(Notificacao notificacao);
+    }
+}

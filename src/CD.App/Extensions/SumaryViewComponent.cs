e46f01a0d1248c3711ee2ad088e68ef79310e8c4
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-09-22 12:07:49 -0300

    Created extension SumaryViewComponent

---
+++ src/CD.App/Extensions/SumaryViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using CD.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Text;
+
+namespace CD.App.Extensions
+{
+    public class SumaryViewComponent : ViewComponent
+    {
+        private readonly INotificador _notificador;
+
+        public SumaryViewComponent(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var notificacoes = await Task.FromResult(_notificador.ObterNotificacoes());
+
+            notificacoes.ForEach(c => ViewData.ModelState.AddModelError(string.Empty, c.Mensagem));
+
+            return View();
+        }
+    }
+}

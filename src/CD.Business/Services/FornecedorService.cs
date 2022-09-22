57dcc1f265284c93c6f451def1d96db8b120fc46
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-09-22 10:01:36 -0300

    Including constructor on FornecedorService and updated task Adicionar

--- src/CD.Business/Services/FornecedorService.cs
+++ src/CD.Business/Services/FornecedorService.cs
@@ -6,10 +6,28 @@ namespace CD.Business.Services
 {
     public class FornecedorService : BaseService, IFornecedorService
     {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IEnderecoRepository _enderecoRepository;
+
+        public FornecedorService(IFornecedorRepository fornecedorRepository, IEnderecoRepository enderecoRepository)
+        {
+            _fornecedorRepository = fornecedorRepository;
+            _enderecoRepository = enderecoRepository;
+        }
+
         public async Task Adicionar(Fornecedor fornecedor)
         {
             if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)
                 && !ExecutarValidacao(new EnderecoValidation(), fornecedor.Endereco)) return;
+
+            if (_fornecedorRepository.Buscar(f => f.Documento == fornecedor.Documento).Result.Any())
+            {
+                Notificar("Já existe um fornecedor com este documento informado");
+                return;
+            }
+
+            await _fornecedorRepository.Adicionar(fornecedor);
+            return;
         }
 
         public async Task Atualizar(Fornecedor fornecedor)

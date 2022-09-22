9adba0dd84d35c0f0459db29cc3253f00da9f07d
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-09-22 10:23:40 -0300

    Injecting ProdutoRepository on ProdutoService and updated its methods

--- src/CD.Business/Services/ProdutoService.cs
+++ src/CD.Business/Services/ProdutoService.cs
@@ -6,19 +6,30 @@ namespace CD.Business.Services
 {
     public class ProdutoService : BaseService, IProdutoService
     {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoService(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
         public async Task Adicionar(Produto produto)
         {
             if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+
+            await _produtoRepository.Adicionar(produto);
         }
 
         public async Task Atualizar(Produto produto)
         {
             if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+
+            await _produtoRepository.Atualizar(produto);
         }
 
         public async Task Remover(Guid id)
         {
-            throw new NotImplementedException();
+            await _produtoRepository.Remover(id); 
         }
     }
 
